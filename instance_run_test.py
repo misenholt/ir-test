071f09da323ee5a5841f7ccff024f72af6223e2d
Max Isenholt <misenholt@illinoisrocstar.com>
2018-10-08 15:36:06 -0500

    update script

--- instance_run_test.py
+++ instance_run_test.py
@@ -1,10 +1,16 @@
-import datetime
-import re
+import requests
 
 if __name__ == '__main__':
-    ct = datetime.datetime.now()
-    ct = re.sub(r'[\.: -]', '_', str(ct))
+    # file_path = '512MB.zip'
+    file_path = 'test.file'
+    data = {'token': 'testtoken'}
+    url = 'http://staging.api.simverse.com/v1/files'
 
-    with open('./{}.log'.format(ct), 'w') as log:
-        log.write('...')
+    with open(file_path, 'r') as up_file:
+        files = {'file': up_file}
+        response = requests.put(url,
+                                files=files,
+                                data=data
+                                )
+        print(response.status_code)
 
