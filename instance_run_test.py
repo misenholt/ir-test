b8e40ed365a7a4bc470bfb7fd26a9ff8f6eaea73
Max Isenholt <misenholt@illinoisrocstar.com>
2018-10-08 13:20:17 -0500

    update script

--- instance_run_test.py
+++ instance_run_test.py
@@ -1,9 +1,10 @@
 import datetime
+import re
 
 if __name__ == '__main__':
     ct = datetime.datetime.now()
+    ct = re.sub(r'[\.: -]', '_', str(ct))
 
-    with open('/home/ubuntu/test.log', 'w') as log:
+    with open('./{}.log'.format(ct), 'w') as log:
         log.write('...')
-        log.write(str(ct))
 
