ca2acaba224946eb7d2d88be0fdd42119ecfcc98
Max Isenholt <misenholt@illinoisrocstar.com>
2018-10-08 12:56:52 -0500

    update script

--- instance_run_test.py
+++ instance_run_test.py
@@ -3,7 +3,7 @@ import datetime
 if __name__ == '__main__':
     ct = datetime.datetime.now()
 
-    with open('/home/ubuntu/test.log') as log:
+    with open('/home/ubuntu/test.log', 'w') as log:
         log.write('...')
         log.write(str(ct))
 
